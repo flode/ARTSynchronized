85642db76e10e506c124ade22e275c5d8e6b9775
Florian Scheibner <flo.de@hotmail.de>
2016-08-30 08:02:21 -0700

    Add example for single threaded ART

--- example.cpp
+++ example.cpp
@@ -15,11 +15,76 @@ void loadKey(TID tid, Key &key) {
     reinterpret_cast<uint64_t *>(&key[0])[0] = __builtin_bswap64(tid);
 }
 
-int main(int argc, char **argv) {
-    if (argc != 3) {
-        printf("usage: %s n 0|1|2\nn: number of keys\n0: sorted keys\n1: dense keys\n2: sparse keys\n", argv[0]);
-        return 1;
+void singlethreaded(char **argv) {
+    std::cout << "single threaded:" << std::endl;
+
+    uint64_t n = std::atoll(argv[1]);
+    uint64_t *keys = new uint64_t[n];
+
+    // Generate keys
+    for (uint64_t i = 0; i < n; i++)
+        // dense, sorted
+        keys[i] = i + 1;
+    if (atoi(argv[2]) == 1)
+        // dense, random
+        std::random_shuffle(keys, keys + n);
+    if (atoi(argv[2]) == 2)
+        // "pseudo-sparse" (the most-significant leaf bit gets lost)
+        for (uint64_t i = 0; i < n; i++)
+            keys[i] = (static_cast<uint64_t>(rand()) << 32) | static_cast<uint64_t>(rand());
+
+    printf("operation,n,ops/s\n");
+    ART_unsynchronized::Tree tree(loadKey);
+
+    // Build tree
+    {
+        auto starttime = std::chrono::system_clock::now();
+        for (uint64_t i = 0; i != n; i++) {
+            Key key;
+            loadKey(keys[i], key);
+            tree.insert(key, keys[i]);
+        }
+        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(
+                std::chrono::system_clock::now() - starttime);
+        printf("insert,%ld,%f\n", n, (n * 1.0) / duration.count());
     }
+
+    {
+        // Lookup
+        auto starttime = std::chrono::system_clock::now();
+        for (uint64_t i = 0; i != n; i++) {
+            Key key;
+            loadKey(keys[i], key);
+            auto val = tree.lookup(key);
+            if (val != keys[i]) {
+                std::cout << "wrong key read: " << val << " expected:" << keys[i] << std::endl;
+                throw;
+            }
+        }
+        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(
+            std::chrono::system_clock::now() - starttime);
+        printf("lookup,%ld,%f\n", n, (n * 1.0) / duration.count());
+    }
+
+    {
+        auto starttime = std::chrono::system_clock::now();
+
+        for (uint64_t i = 0; i != n; i++) {
+            Key key;
+            loadKey(keys[i], key);
+            tree.remove(key, keys[i]);
+        }
+        auto duration = std::chrono::duration_cast<std::chrono::microseconds>(
+                std::chrono::system_clock::now() - starttime);
+        printf("remove,%ld,%f\n", n, (n * 1.0) / duration.count());
+    }
+    delete[] keys;
+
+    std::cout << std::endl;
+}
+
+void multithreaded(char **argv) {
+    std::cout << "multi threaded:" << std::endl;
 
     uint64_t n = std::atoll(argv[1]);
     uint64_t *keys = new uint64_t[n];
@@ -92,5 +157,17 @@ int main(int argc, char **argv) {
         printf("remove,%ld,%f\n", n, (n * 1.0) / duration.count());
     }
     delete[] keys;
+}
+
+int main(int argc, char **argv) {
+    if (argc != 3) {
+        printf("usage: %s n 0|1|2\nn: number of keys\n0: sorted keys\n1: dense keys\n2: sparse keys\n", argv[0]);
+        return 1;
+    }
+
+    singlethreaded(argv);
+
+    multithreaded(argv);
+
     return 0;
 }
